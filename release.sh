150ca2c2e22d3febf6e11d10437ed31cce8ec711
Carlos Abraham <abraham@19cah.com>
2018-06-28 13:06:06 -0400

    changes

--- release.sh
+++ release.sh
@@ -5,20 +5,16 @@ if ([ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ])
     echo "please specify one of (major, minor, patch)"
     exit
 fi
-
+#
 git checkout master
-npm install
+# npm install
+# npm version $1
+# git add .
+#
+# git commit -m "$PACKAGE_VERSION published"
+# git push origin master
+# npm publish
+# git tag -a $PACKAGE_VERSION -m  "$PACKAGE_VERSION published"
+# git push origin --tags
+git clean -ndx
 npm version $1
-git add .
-# Version key/value should be on his own line
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
-echo $PACKAGE_VERSION
-git commit -m "$PACKAGE_VERSION published"
-git push origin master
-npm publish
-git tag -a $PACKAGE_VERSION -m  "$PACKAGE_VERSION published"
-git push origin --tags
