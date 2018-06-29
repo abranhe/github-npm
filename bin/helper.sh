08939a66ff39f9cc984a812a5877ffb22056c1ce
Carlos Abraham <abraham@19cah.com>
2018-06-29 01:40:01 -0400

    major change init

---
+++ bin/helper.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+# MIT (c) 2018 Carlos Abraham Hernandez
+#
+# helper.sh
+#
+# Easy way to release npm packages on Github
+# Issues: https://github.com/19cah/github-npm/issues
+
+# Args
+first=$1
+second=$2
+third=$3
+all=${@:2}
+
+# Colors
+RESET="\033[0m"
+REVERSE="\033[07m"
+PINK="\033[95m"
+GREEN="\033[1;32m"
+BLUE="\033[01;34m"
+WHITE="\033[1;37m"
+RED='\033[0;31m'
+DARKRED="\033[1;31m"
+
+function help()
+{
+	echo -e "
+${PINK}Usage${RESET}:
+	${PINK}${REVERSE}$ npx github-npm <command...>${RESET}
+
+${PINK}Where${RESET} command is:
+	${WHITE}-h  help          show help${RESET}
+	${GREEN}<version>${RESET}         avilable [<major>,<minor>, <patch>]
+	-m  <message>     custom message
+	--tag  <tag>      custom tag value
+	--version         package version
+"
+}
--- release.sh
+++ release.sh
@@ -8,38 +8,76 @@
 # Issues: https://github.com/19cah/github-npm/issues
 
 
-if ([ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ])
-  then
-    echo "please specify one of (major, minor, patch)"
-    exit
+# Helper
+source "bin/helper.sh"
+
+# Commands
+COMMANDS=(
+    help    #0
+		-h   	  #1
+		major   #2
+		minor		#3
+		patch		#4
+		--tag   #5
+		-m			#6
+)
+
+# Check for invalid arguments
+if [[ ! " ${COMMANDS[@]} " =~ " ${first} " ]]; then
+    echo -e "${RED}github-npm: '${first}' is not a valid command. ${RESET}" # "$(test ! -z " $all" -a "$all" != " " && echo "\nParameters: $all" || echo "") ${RESET}"
+		echo "See 'github-npm -h' for more information"
+		exit 0
 fi
 
-git checkout master
-git add .
-git commit -m 'Save all to publish package'
+# When user ask for help
+if (( " ${COMMANDS[0]} " == "${first}" )) ||  (( " ${COMMANDS[1]} " == "${first}" )); then
+	echo -e "$(help)"
+else
+	echo "no se q pasa "
+fi
 
-npm install
-npm version $1
+# # If user enter verson command
+# if (( " ${COMMANDS[2]} " =~ "${first}" )) ||  (( " ${COMMANDS[3]} " =~ "${first}" )) || (( " ${COMMANDS[4]} " =~ "${first}" )); then
+#     echo "${first}"
+# fi
 
-# Getting package version
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
 
-echo $PACKAGE_VERSION
+# Ask user
+# read -p "read line  " read
+# echo "${read}"
 
-# Add all
-git add .
-git commit -m "$PACKAGE_VERSION published"
-
-# Publish package
-npm publish
-
-# Push commits
-git push origin master
-
-# Tag version
-git tag -a "v$PACKAGE_VERSION" -m  "Welcome to $PACKAGE_VERSION version"
-git push origin --tags
+# if ([ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ])
+#   then
+#     echo "please specify one of (major, minor, patch)"
+#     exit
+# fi
+#
+# git checkout master
+# git add .
+# git commit -m 'Save all to publish package'
+#
+# npm install
+# npm version $1
+#
+# # Getting package version
+# PACKAGE_VERSION=$(cat package.json \
+#   | grep version \
+#   | head -1 \
+#   | awk -F: '{ print $2 }' \
+#   | sed 's/[",]//g')
+#
+# echo $PACKAGE_VERSION
+#
+# # Add all
+# git add .
+# git commit -m "$PACKAGE_VERSION published"
+#
+# # Publish package
+# npm publish
+#
+# # Push commits
+# git push origin master
+#
+# # Tag version
+# git tag -a "v$PACKAGE_VERSION" -m  "Welcome to $PACKAGE_VERSION version"
+# git push origin --tags
