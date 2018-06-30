fc726d38a5032face0e89e2caebe63acbc2c5d44
Carlos Abraham <abraham@19cah.com>
2018-06-30 02:59:30 -0400

    fix usage

--- bin/helper.sh
+++ bin/helper.sh
@@ -48,8 +48,8 @@ ${PINK}Where${RESET} command is:
 	${GREEN}-h  help${RESET}                     show help
 	${GREEN}--version${RESET}                    view package version
 	${GREEN}<version>${RESET}                    select between <patch, minor, major>
-	${GREEN}<version> -m \"message\"${RESET}     add a custom message to commit
-	                             Eg: $ github-npm patch -m "drop new realese"
+	${GREEN}<version> -m <message>\"${RESET}     add a custom message to commit
+	                             Eg: $ github-npm patch -m \"drop new realese\"
 "
 }
 
